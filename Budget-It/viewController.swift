506256b0dd0c4cc4104002c34933697300009cbb
Frank Dong <FrankyDStar@hotmail.com>
2017-11-19 06:16:50 -0500

    Final

--- Budget-It/viewController.swift
+++ Budget-It/viewController.swift
@@ -1,35 +1,26 @@
 //
-//  SecondViewController.swift
+//  FirstViewController.swift
 //  Budget-It
 //
 //  Created by Weija Zhou on 2017-11-18.
 //  Copyright © 2017 Weija Zhou. All rights reserved.
 //
 import UIKit
-import SafariServices
 
-
-
-class linktowebController: UIViewController {
+class viewController: UIViewController {
     
+    @IBOutlet weak var myWebView: UIWebView!
     override func viewDidLoad() {
+        print("HELELFL")
+        myWebView.loadRequest(URLRequest(url: URL(string: "http://hardwirestudios.com")!))
         super.viewDidLoad()
-        
+        //Uncomment the line below if you want the tap n
     }
-    
-    
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    //    override func viewDidDisappear(_ animated: Bool) {
-    //
-    //    }
-    //
-    //
-    
-    
-    
 }
+
+
